2caf72eb931d8f2381e711c9d33b388bb118fac7
racky <rackybarrodiatta@gmail.com>
2024-05-17 23:31:32 +0000

    ajout de quelles fonctionnalites

--- dom.js
+++ dom.js
@@ -1,8 +1,17 @@
+
+//recuperer depuis le local storage 
+function getlivres (){
+  return  JSON.parse(localStorage.getItem('livres'))
+}
+
+
+
+
 // contenu du tableau 
-const livres =[
-   { id : "","titre" : "sous l orage " , "auteur" : "seydou badiane" ,  } ,
-   { id : "","titre" : " testaman" , "auteur" : "Aminata sow fall" ,  }, 
-   { id : "","titre" : "l amour" , "auteur" : "Fatou ndiaye" ,  }, 
+const initialesLivres = getlivres () || [
+   { id : "122244" , "titre" : "sous l orage " , "auteur" : "seydou badiane"} ,
+   {  id : "1358699" ,"titre" : " testaman" , "auteur" : "Aminata sow fall"}, 
+   {  id : "1258888" ,"titre" : "l amour" , "auteur" : "Fatou ndiaye"}, 
 ]
 const table = document.querySelector(".table");
 const tblBody = document.createElement("tbody");
@@ -12,25 +21,57 @@ const countElement = document.querySelector(".count");
 function setCount(count){
     countElement.innerHTML = count ;
 }
+//ajouter dans localstorage
+function setlivres (livres){
+  localStorage.setItem('livres' ,JSON.stringify(livres))
+}
+
+setlivres(initialesLivres)
+let livres = getlivres ()
+function generer () {
+const id = new Date().getTime()
+idArray.push(nouvelID)
+}
 
  function createtable(){
-    const id = new Date().getTime();
-   
-     for ( let index = 0 ; index < 3 ; index++){
+    for ( let index = 0 ; index < livres.length ; index++){
+      
         let row = document.createElement("tr")
+        
+         
+        //creation button lu
+        //
+       
         // creation des bouttons suppression
         let buttonCell= document.createElement("td")
+        let buttonlu=document.createElement("td")
+        let luButton=document.createElement("button")
+        let buttontext = document.createTextNode("lu")
+        luButton.setAttribute("class","lu")
+        luButton.appendChild(buttontext)
         let deleteButton = document.createElement("button")
         let buttonText = document.createTextNode("supprimer")
-        deleteButton.setAttribute("class","delete-btn")
+       deleteButton.setAttribute("class","delete")
         deleteButton.appendChild(buttonText)
-  for (let element = 0; element < livres.length; element ++){
+
+         
+ 
+        
+        
+     
+  for (let element = 0; element < Object.keys(livres[0]).length; element ++){
     const cell = document.createElement("td") 
-   const cellText =document.createTextNode(Object.values (livres[index])[element ])
+   const cellText =document.createTextNode(Object.values (livres[index])[element])
+   deleteButton.setAttribute("titre" , (livres[index]).titre)
    buttonCell.appendChild(deleteButton)
+   buttonlu.appendChild(luButton)
      cell.appendChild(cellText)
      row.appendChild(cell)
      row.appendChild(buttonCell)
+     row.appendChild(buttonlu)
+     row.setAttribute('id' , (livres[index]).titre)
+     row.setAttribute('id' , (livres[index]).titre)
+    
       }
       tblBody.appendChild(row)
      }
@@ -38,7 +79,6 @@ function setCount(count){
      document.body.appendChild(table)
  }
  createtable() 
-setCount(0)
 
 
 /* modal*/
@@ -54,73 +94,133 @@ close.addEventListener('click',()=>{
 })
 
 
+   //suppression
 
 
+  
+// ajouter un livre
+let addButton = document.querySelector(".addButton")
+addButton.onclick = function (){
+  event.preventDefault()
+  //const time = document.getElementById("time").value
+ const text = document.getElementById("text").value
+ const auteur = document.getElementById("auteur").value
+if(  !text|| ! auteur  ){
+  alert("merci de tout remplir")
+  return
+}
+const newlivre = {text, auteur}
+livres.push(newlivre)
+//console.log('livres' , livres)
+setlivres(livres)
 
+// ajouter un tr
+let row = document.createElement("tr")
+let cell0 = row.insertCell(0)
+//const cell0Text = document.createTextNode(time)
+//cell0.appendChild(cell0Text)
+//row.appendChild(cell0)
+let cell1 = row.insertCell(1)
+const cell1Text = document.createTextNode(text)
+cell1.appendChild(cell1Text)
+row.appendChild(cell1)
+let cell2 = row.insertCell(2)
+const cell2Text = document.createTextNode(auteur)
+cell2.appendChild(cell2Text)
+row.appendChild(cell2)
+// creation des buttons 
+let buttonCell= document.createElement("td")
+let deleteButton = document.createElement("button")
+let buttonText = document.createTextNode("supprimer")
+deleteButton.setAttribute("class","delete")
 
+deleteButton.appendChild(buttonText)
+buttonCell.appendChild(deleteButton)
+row.appendChild(buttonCell)
+let buttonlu=document.createElement("td")
+let luButton=document.createElement("button")
+let buttontext = document.createTextNode("lu")
+luButton.setAttribute("class","lu")
+luButton.appendChild(buttontext)
+buttonlu.appendChild(luButton)
+row.appendChild(buttonlu)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*var alire = document.getElementById("alire")
-var lu = document.getElementById("lu")
-document.getElementById('add').addEventListener('click',function(){
-
-var value = document.getElementById("item").value;
-
-
-alire.insertAdjacentHTML("afterbegin",` <li>${value}<i class="far fa-check-square" id=${value} onClick=marquerLu(this.id)></i><i class="fas fa-trash" id=${value} onClick=supprimer(this.id)></i></li>`);
-
-
+//ajouter un evenement
+deleteButton.addEventListener('click' , function (){
+  const titre = this.getAttribute('titre')
+  let row = document.getElementById(titre)
+     row.parentNode.removeChild(row)
+    // enlever l element supprimer
+let  filteredLivres = livres.filter(
+  (livre) => livre.titre!==titre
+)
+  livres = filteredLivres
+//setCount(livres.length)
 
 })
 
-function supprimer(id){
+row.setAttribute('id' , text )
+tblBody.appendChild(row)
+table.appendChild(tblBody)
+document.body.appendChild(table)
+// vider les inputs 
+document.getElementById('text').value=''
+document.getElementById('auteur').value=''
+}
+window.onload = function() {
+  let deleteButton = document.querySelectorAll('.delete')
+ //console.log("deleteButton" , deleteButton )
+   deleteButton.forEach(function (button){ 
+      button.addEventListener('click', function(){
+      const titre = this.getAttribute("titre")
+     let row = document.getElementById(titre)
+      //console.log("row",row )
+    row.parentNode.removeChild(row)
+    // enlever l element supprimer
+let  filteredLivres = livres.filter((livre)=>livre.titre!==titre)
+     livres = filteredLivres
+setCount(livres.length)
+
+
+    
+  })
+   })
+   
+};
+
+  const lu = document.querySelectorAll('.lu')
+  const row = document.querySelectorAll('.titre')
+  lu.forEach(function (button){
+    button.addEventListener('click',function(){
+      //const titre = this.setAttribute ('titre')
+      //let row = document.getElementById(titre)
+    row.style.textDecoration="line-through"
+      
+  })
   
- document.getElementById(id).parentElement.outerHTML = "";
-
-}
-
-function marquerLu(id){
-    supprimer(id)
-    lu.insertAdjacentHTML("afterbegin",` <li>${id}</i><i class="fas fa-trash" id=${id} onClick=supprimer(this.id)></i></li>`);
-
-    console.log(id)
-}
-var alire = document.getElementById("alire")
-var lu = document.getElementById("lu")
-document.getElementById('add').addEventListener('click',function(){
-
-var value = document.getElementById("item").value;
-
-
-alire.insertAdjacentHTML("afterbegin",` <li>${value}<i class="far fa-check-square" id=${value} onClick=marquerLu(this.id)></i><i class="fas fa-trash" id=${value} onClick=supprimer(this.id)></i></li>`);
-
-
-
 })
 
-function supprimer(id){
-  
- document.getElementById(id).parentElement.outerHTML = "";
+setCount(3)
 
-}
 
-function marquerLu(id){
-    supprimer(id)
-    lu.insertAdjacentHTML("afterbegin",` <li>${id}</i><i class="fas fa-trash" id=${id} onClick=supprimer(this.id)></i></li>`);
 
-    console.log(id)
-}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
